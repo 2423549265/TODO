48bed06975eed51af5918831c0250eac40953935
2423549265 <2423549265@qq.com>
2017-04-22 21:17:59 +0800

    Finished git

--- www/js/controllers/TasksController.js
+++ www/js/controllers/TasksController.js
@@ -1,4 +1,9 @@
 angular.module('starter.controllers')
 .controller('TasksController', function ($rootScope, $scope, Tasks) {
     $scope.tasks = Tasks.all();
+    $scope.complete_tasks = Tasks.complete_all();
+    $scope.complete = function (task) {
+        //alert("complete");
+        Tasks.complete(task);
+    };
 });
--- www/js/services.js
+++ www/js/services.js
@@ -8,6 +8,13 @@ angular.module('starter.services')
             }
             return [];
         },
+        complete_all: function(){
+            var complete_taskString = window.localStorage['complete_tasks'];
+            if (complete_taskString) {
+                return angular.fromJson(complete_taskString);
+            }
+            return [];
+        },
         save: function (task) {
             var tasks = [];
             var taskString = window.localStorage['tasks'];
@@ -16,6 +23,30 @@ angular.module('starter.services')
             }
             tasks.push(task);
             window.localStorage['tasks'] = angular.toJson(tasks);
+        },
+        complete: function (task) {
+            //console.log(task.title);
+            var tasks = [];
+            var taskString = window.localStorage['tasks'];
+            if (taskString) {
+                tasks = angular.fromJson(taskString);
+            }
+            for (var i = 0; i < tasks.length; i++) {
+                if (tasks[i].title == task.title)
+                    tasks.splice(i, 1);
+            }
+            //tasks.push(task);
+            window.localStorage['tasks'] = angular.toJson(tasks);
+
+            var complete_tasks = [];
+            var complete_taskString = window.localStorage['complete_tasks'];
+            if (complete_taskString) {
+                complete_tasks = angular.fromJson(complete_taskString);
+            }
+            complete_tasks.push(task);
+            window.localStorage['complete_tasks'] = angular.toJson(complete_tasks);
+
+            window.location.reload();
         }
     }
 })
